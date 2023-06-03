63ad0f7fa0f7951703182030909eefc2ab0af845
Ragnarsmoke <emil.bertilsson@outlook.com>
2023-06-03 14:01:39 +0200

    Base setup

--- keyset.py
+++
@@ -1,8 +0,0 @@
-import curses
-
-keyset_up = [curses.KEY_UP, 16, 107, 75] # ^P k K
-keyset_down = [curses.KEY_DOWN, 14, 106, 74] # ^N j J
-keyset_left = [curses.KEY_LEFT, 2, 104, 72] # ^B h H
-keyset_right = [curses.KEY_RIGHT, 6, 108, 76] # ^F l L
-keyset_enter = [curses.KEY_ENTER, 10]
-keyset_esc = [27]
